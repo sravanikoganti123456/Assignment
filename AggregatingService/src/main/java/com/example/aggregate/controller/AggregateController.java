29653cfa6c40ac8496217872da917653a152c57a
sravanikoganti123456 <31911162+sravanikoganti123456@users.noreply.github.com>
2022-08-12 20:42:16 +0530

    Update AggregateController.java

--- AggregatingService/src/main/java/com/example/aggregate/controller/AggregateController.java
+++ AggregatingService/src/main/java/com/example/aggregate/controller/AggregateController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.context.annotation.ComponentScan;
 import com.example.aggregate.model.Population;
 
 @RestController
