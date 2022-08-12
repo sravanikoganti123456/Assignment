07102d36d53070f4c5fb23e33a692b354e906a19
sravanikoganti123456 <31911162+sravanikoganti123456@users.noreply.github.com>
2022-08-12 20:37:29 +0530

    Update AggregateController.java

--- AggregatingService/src/main/java/com/example/aggregate/controller/AggregateController.java
+++ AggregatingService/src/main/java/com/example/aggregate/controller/AggregateController.java
@@ -39,4 +39,26 @@ public class AggregateController {
 	}
 	
 	
+		// retrieve all population data from 3 countries
+		@GetMapping("getPopulationFromCountryService")
+		public Population getAllStudent123()
+		{
+			RestTemplate restTemplate=new RestTemplate();
+			ResponseEntity<Population> result1 = restTemplate.getForEntity("http://localhost:8080/country/getCanadaPopulation", Population.class);
+			ResponseEntity<Population> result2 = restTemplate.getForEntity("http://localhost:8080/country/getFrancePopulation", Population.class);
+			ResponseEntity<Population> result3 = restTemplate.getForEntity("http://localhost:8080/country/getGermanyPopulation", Population.class);
+			
+			int totalpopulation=result1.getBody().getTotalPopulation() + result2.getBody().getTotalPopulation() + result3.getBody().getTotalPopulation();
+			int malepopulation=result1.getBody().getTotalMalePopulation() + result2.getBody().getTotalMalePopulation() + result3.getBody().getTotalMalePopulation();
+			int femalepopulation= result1.getBody().getTotalFemalePopulation() + result2.getBody().getTotalFemalePopulation() + result3.getBody().getTotalFemalePopulation();
+			
+			Population total=new Population();
+			total.setTotalPopulation(totalpopulation);
+			total.setTotalMalePopulation(malepopulation);
+			total.setTotalFemalePopulation(femalepopulation);
+			
+			return total;			
+		}
+	
+	
 }
